e39ec5afe6c266ed7b73c5fa029001dd15c2398a
NicoMaker <nicola.marano02@gmail.com>
2025-09-26 14:32:22 +0200

    dati

---
+++ Lezioni/4_Lezione/Esercizi/frontend/script.js
@@ -0,0 +1,7 @@
+document.getElementById("btnProdotti").addEventListener("click", () => {
+  console.log("Hai cliccato su: Elenco Prodotti");
+});
+
+document.getElementById("btnOrdine").addEventListener("click", () => {
+  console.log("Hai cliccato su: Nuovo Ordine");
+});
